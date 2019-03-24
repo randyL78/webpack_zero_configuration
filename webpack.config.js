430afd1e8d67357bc72ea3163add74c457c7a13b
Randy's Mac <randydavidl78@gmail.com>
2019-03-24 18:47:42 -0400

    add babel to webpack config

---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+const webpackconfig = {
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: "babel-loader"
+      }
+    ]
+  }
+};
+
+module.exports = webpackconfig;
